e376626932771b0b2f6e0a51bebca290fb2d24b5
PVHG003 <phamhung155tp@gmail.com>
2025-03-16 18:37:46 +0700

    Update: Change Relationship Between Game and Category, Game and Platform to n-n

--- src/main/java/vn/pvhg/leaderboard/model/Game.java
+++ src/main/java/vn/pvhg/leaderboard/model/Game.java
@@ -5,7 +5,6 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
@@ -28,7 +27,24 @@ public class Game {
 
     private String description;
 
-    private String coverImageUrl; // URL to a cover image
+    private String coverImageUrl;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "game_platforms",
+            joinColumns = @JoinColumn(name = "game_id"),
+            inverseJoinColumns = @JoinColumn(name = "platform_id")
+    )
+    private Set<Platform> platforms;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "game_categories",
+            joinColumns = @JoinColumn(name = "game_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories;
+
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
@@ -37,4 +53,5 @@ public class Game {
     @UpdateTimestamp
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+
 }
