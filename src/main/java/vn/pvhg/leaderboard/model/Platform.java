a57cb07302b5902a96e76c335589b8477e672c84
PVHG003 <phamhung155tp@gmail.com>
2025-03-16 18:39:43 +0700

    Added Platform Entity

---
+++ src/main/java/vn/pvhg/leaderboard/model/Platform.java
@@ -0,0 +1,28 @@
+package vn.pvhg.leaderboard.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+
+@Entity
+@Table(name = "platforms")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Platform {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "platform_type", nullable = false)
+    private String platformType;
+
+    @ManyToMany(mappedBy = "platforms", fetch = FetchType.LAZY)
+    private Set<Game> games;
+}
